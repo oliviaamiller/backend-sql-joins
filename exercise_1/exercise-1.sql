48fa62e19ce4c103844828adf12b48f840ad0584
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-03-21 12:28:23 -0700

    exercise 1 test passing

--- exercise_1/exercise-1.sql
+++ exercise_1/exercise-1.sql
@@ -1,16 +1,12 @@
 -- find all address_id of addresses in the city of Dundee
 -- use INNER JOIN FROM address on to city
-
-DROP TABLE IF EXISTS city CASCADE;
-DROP TABLE IF EXISTS address CASCADE;
-
-CREATE TABLE city (
-    city_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    city TEXT NOT NULL,
-);
-
-CREATE TABLE IF EXISTS address (
-    address_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    city_id BIGINT
-    FOREIGN KEY (city_id) REFERENCES city(city_id)
-);
+SELECT
+	address_id
+FROM 
+	address
+INNER JOIN
+	city
+ON
+	city.city_id = address.city_id
+WHERE 
+	city.city = 'Dundee';
